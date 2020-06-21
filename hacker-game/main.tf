6bae560f6463819fc5a2eabeb71be156b26384af
tabramczyk <sinithr@gmail.com>
2020-06-21 13:01:02 +0200

    Dynamodb users table.

---
+++ hacker-game/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+   region  = var.core_region
+   version = "~> 2.5"
+}
+
+resource "aws_dynamodb_table" "users" {
+  name           = "users" 
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "username"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "UsernameIndex"
+    hash_key           = "username"
+    projection_type    = "ALL"
+  }
+}
